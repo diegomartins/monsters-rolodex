a2613bc1ae97859762636d2d6aa98f9b5deba39d
Diego Martins <diegomartins@github.com>
2020-03-31 21:30:04 +1100

    Updates

---
+++ precache-manifest.675c722dd3c97ceaedc824fd6468cc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cdafae90613db5514d6021b30c6d7b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a0257cfbed44cf86cf44",
+    "url": "/monsters-rolodex/static/css/main.afd57542.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0257cfbed44cf86cf44",
+    "url": "/monsters-rolodex/static/js/main.b822795a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
